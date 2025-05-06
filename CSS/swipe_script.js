a6af8d6c3e07ac584acc15100f49ad850b587cab
precambrien <precambrien@cestcool.net>
2025-05-06 02:17:46 +0200

    mp

--- CSS/swipe_script.js
+++ CSS/swipe_script.js
@@ -4,12 +4,12 @@
 
   function handleGesture() {
     if (touchEndX < touchStartX - 50) {
-      // swipe left → go forward
+      // swipe left : go forward
       const right = document.querySelector('.arrow.right');
       if (right) right.click();
     }
     if (touchEndX > touchStartX + 50) {
-      // swipe right → go back
+      // swipe right : go back
       const left = document.querySelector('.arrow.left');
       if (left) left.click();
     }
